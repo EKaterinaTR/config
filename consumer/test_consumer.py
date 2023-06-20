7a4a0175739ec30c4ad5b74d3673efb356cf0eb3
EKaterina Tsareva <ekaterinatsr@gmail.com>
2023-06-20 12:28:12 +0300

    fix consumer

--- consumer/test_consumer.py
+++ consumer/test_consumer.py
@@ -4,16 +4,14 @@ from datetime import datetime
 from kafka import KafkaConsumer
 
 def messages(from_connection, checker):
-   subscription = get_subscription()
    for message in from_connection:
       print(message)
 
 
 if __name__ == '__main__':
    try:
-       checker = Checker('request_to_course')
        consumer = KafkaConsumer('request_to_course',
-                                bootstrap_servers=['51.250.2.37:29092'],
+                                bootstrap_servers=['localhost:29092'],
                                 auto_offset_reset='earliest')
        messages(consumer, to_connection,checker)
 
